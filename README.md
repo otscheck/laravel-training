23c30428343503cabd42e131f5731d675b11feeb
olivier otschudi omanga <olivier.otschudi@gmail.com>
2023-09-28 13:12:24 +0100

    fin du projet udemy-one

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Laravel 10 : (Udemy One)
 
 ## Training sur les permissions
+
+### fini le 28/09/23
---
+++ database/seeders/Admin.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class Admin extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::create([
+            'name'=>'admin',
+            'email'=>'admin@email.com',
+            'email_verified_at'=>now(),
+            'password'=>bcrypt('password'),
+            'is_admin'=>1
+        ]);
+    }
+}
